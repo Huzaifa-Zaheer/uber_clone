6f26004f60d0ab28395de5e80cde6d7929d5e962
Huzaifa-Zaheer <mhuzaifazaheer@gmail.com>
2025-02-09 16:53:22 +0500

    Captain done

--- backend/routes/captain.routes.js
+++ backend/routes/captain.routes.js
@@ -2,6 +2,7 @@ const captainController = require('../controllers/captain.controller');
 const express = require('express');
 const router = express.Router();
 const { body } = require('express-validator');
+const authMiddleware = require('../middlewares/auth.middleware');
 
 
 router.post('/register', [
@@ -13,6 +14,18 @@ router.post('/register', [
     body('vehicle.vehicleType').isIn([ 'car', 'motorbike', 'auto' ]).withMessage('Vehicle should be car, motorbike or auto'),
     ], captainController.registerCaptain
 );
+
+
+router.post('/login', [
+    body('email').isEmail().withMessage({ message: 'Invalid Email' }),
+    body('password').isLength({ min: 6 }).withMessage({ message: 'Passwod must be atleast 6 characters long' }),
+    ], captainController.loginCaptain
+)
+
+
+router.get('/profile', authMiddleware.authCaptain, captainController.getCaptainProfile)
+
+router.get('/logout', authMiddleware.authCaptain, captainController.logoutCaptain)
 
 
 
